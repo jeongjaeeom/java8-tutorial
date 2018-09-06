401c6841a7f528af4f97ebdc31b2fa963d56a0c8
jjeom87 <jjeom87@gmail.com>
2018-09-06 12:40:06 +0900

    Fix

--- src/com/winterbe/java8/samples/stream/Streams1.java
+++ src/com/winterbe/java8/samples/stream/Streams1.java
@@ -9,94 +9,81 @@ import java.util.Optional;
  */
 public class Streams1 {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        List<String> stringCollection = new ArrayList<>();
-        stringCollection.add("ddd2");
-        stringCollection.add("aaa2");
-        stringCollection.add("bbb1");
-        stringCollection.add("aaa1");
-        stringCollection.add("bbb3");
-        stringCollection.add("ccc");
-        stringCollection.add("bbb2");
-        stringCollection.add("ddd1");
+    List<String> stringCollection = new ArrayList<>();
+    stringCollection.add("ddd2");
+    stringCollection.add("aaa2");
+    stringCollection.add("bbb1");
+    stringCollection.add("aaa1");
+    stringCollection.add("bbb3");
+    stringCollection.add("ccc");
+    stringCollection.add("bbb2");
+    stringCollection.add("ddd1");
+
+    // filtering
+    stringCollection
+      .stream()
+      .filter((s) -> s.startsWith("a"))
+      .forEach(System.out::println);
+    // "aaa2", "aaa1"
 
 
-        // filtering
+    // sorting
+    stringCollection
+      .stream()
+      .sorted()
+      .filter((s) -> s.startsWith("a"))
+      .forEach(System.out::println);
+      // "aaa1", "aaa2"
+    
+    // stringCollection is untouched
+    System.out.println(stringCollection);
+    // ddd2, aaa2, bbb1, aaa1, bbb3, ccc, bbb2, ddd1
+    
+    // mapping
+    stringCollection
+      .stream()
+      .map(String::toUpperCase)
+      .sorted((a, b) -> b.compareTo(a))
+      .forEach(System.out::println);
+    // "DDD2", "DDD1", "CCC", "BBB3", "BBB2", "AAA2", "AAA1"
 
+    // matching
+    boolean anyStartsWithA =
         stringCollection
-                .stream()
-                .filter((s) -> s.startsWith("a"))
-                .forEach(System.out::println);
+          .stream()
+          .anyMatch((s) -> s.startsWith("a"));
+    System.out.println(anyStartsWithA); // true
 
-        // "aaa2", "aaa1"
-
-
-        // sorting
-
+    boolean allStartsWithA =
         stringCollection
-                .stream()
-                .sorted()
-                .filter((s) -> s.startsWith("a"))
-                .forEach(System.out::println);
+          .stream()
+          .allMatch((s) -> s.startsWith("a"));
+    System.out.println(allStartsWithA); // false
 
-        // "aaa1", "aaa2"
-
-
-        // mapping
-
+    boolean noneStartsWithZ =
         stringCollection
-                .stream()
-                .map(String::toUpperCase)
-                .sorted((a, b) -> b.compareTo(a))
-                .forEach(System.out::println);
+        .stream()
+        .noneMatch((s) -> s.startsWith("z"));
+    System.out.println(noneStartsWithZ); // true
 
-        // "DDD2", "DDD1", "CCC", "BBB3", "BBB2", "AAA2", "AAA1"
+    // counting
+    long startsWithB =
+        stringCollection
+          .stream()
+          .filter((s) -> s.startsWith("b"))
+          .count();
+    System.out.println(startsWithB); // 3
 
-
-        // matching
-
-        boolean anyStartsWithA = stringCollection
-                .stream()
-                .anyMatch((s) -> s.startsWith("a"));
-
-        System.out.println(anyStartsWithA);      // true
-
-        boolean allStartsWithA = stringCollection
-                .stream()
-                .allMatch((s) -> s.startsWith("a"));
-
-        System.out.println(allStartsWithA);      // false
-
-        boolean noneStartsWithZ = stringCollection
-                .stream()
-                .noneMatch((s) -> s.startsWith("z"));
-
-        System.out.println(noneStartsWithZ);      // true
-
-
-        // counting
-
-        long startsWithB = stringCollection
-                .stream()
-                .filter((s) -> s.startsWith("b"))
-                .count();
-
-        System.out.println(startsWithB);    // 3
-
-
-        // reducing
-
-        Optional<String> reduced =
-                stringCollection
-                        .stream()
-                        .sorted()
-                        .reduce((s1, s2) -> s1 + "#" + s2);
-
-        reduced.ifPresent(System.out::println);
-        // "aaa1#aaa2#bbb1#bbb2#bbb3#ccc#ddd1#ddd2"
-
-
-    }
+    // reducing
+    Optional<String> reduced =
+        stringCollection
+          .stream()
+          .sorted()
+          .reduce((s1, s2) -> s1 + "#" + s2);
+    reduced.ifPresent(System.out::println);
+    // "aaa1#aaa2#bbb1#bbb2#bbb3#ccc#ddd1#ddd2"
+  }
 
 }
